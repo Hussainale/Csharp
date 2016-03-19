18ce047b55524a1c617e2e46b3ffffba4f940c68
Hussain Ali Arif <hussainaliarif@outlook.com>
2016-03-19 14:40:58 +0500

    Added files via upload
    Create a C# application that will be consisting of list box and combo box controls Fill up the combo box with names of provinces of Pakistan and when the user selects an item from the combo box,it should be removed from the combo box and add it to the list box control and names  and pictures of measure cities of this province should be displayed in the text boxes and picture box respectively.Program should check to ensure that the combo box contains at least one item. If it does not ,display a message box and terminate the program.

---
+++ Task4-Asg1/Task4-Asg1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Task4_Asg1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sindh",
+            "Punjab",
+            "Balochistan",
+            "Azad Kashmir",
+            "Islamabad"});
+            this.comboBox1.Location = new System.Drawing.Point(187, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Proviences Of Pakistan:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(314, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(374, 305);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(445, 324);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(187, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(700, 367);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Task 4";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Task4-Asg1/Task4-Asg1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task4_Asg1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation=@"C:\Users\Hussain Ali Arif\Documents\Visual Studio 2013\Projects\Task4-Asg1\s.jpg";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Sindh")
+            {
+                comboBox1.Items.Remove("Sindh");
+                listBox1.Items.Add("Sindh");
+                textBox1.Text = "Sindh";
+                pictureBox1.ImageLocation = @"C:\Users\Hussain Ali Arif\Documents\Visual Studio 2013\Projects\Task4-Asg1\s.jpg";
+                
+            }
+             if (comboBox1.Text == "Punjab")
+            {
+                comboBox1.Items.Remove("Punjab");
+                listBox1.Items.Add("Punjab");
+                textBox1.Text = "Punjab";
+                pictureBox1.ImageLocation = @"C:\Users\Hussain Ali Arif\Documents\Visual Studio 2013\Projects\Task4-Asg1\1.jpg";
+            }
+             if (comboBox1.Text == "Balochistan")
+            {
+                comboBox1.Items.Remove("Balochistan");
+                listBox1.Items.Add("Balochistan");
+                textBox1.Text = "Balochistan";
+                pictureBox1.ImageLocation = @"C:\Users\Hussain Ali Arif\Documents\Visual Studio 2013\Projects\Task4-Asg1\bol.jpg";
+            }
+             if (comboBox1.Text == "Azad Kashmir")
+            {
+                comboBox1.Items.Remove("Azad Kashmir");
+                listBox1.Items.Add("Azad Kashmir");
+                textBox1.Text = "Azad Kashmir";
+                pictureBox1.ImageLocation = @"C:\Users\Hussain Ali Arif\Documents\Visual Studio 2013\Projects\Task4-Asg1\az.jpg";
+            }
+            if (comboBox1.Text == "Islamabad")
+            {
+                comboBox1.Items.Remove("Islamabad");
+                listBox1.Items.Add("Islamabad");
+                textBox1.Text = "Islamabad";
+                pictureBox1.ImageLocation = @"C:\Users\Hussain Ali Arif\Documents\Visual Studio 2013\Projects\Task4-Asg1\i.jpg";
+            }
+            if (comboBox1.Items.Count == 0)
+            {
+                MessageBox.Show("The List Is Empty");
+                Application.Exit();
+            }        
+        }
+    }
+}
